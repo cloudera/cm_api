972422300bf6b21cd95a99d267bbcb4f1f557ede
Vikram Srivastava <vikrams@cloudera.com>
2012-10-15 17:34:10 -0700

    [py] Make the default version 3 for CM 4.5

--- python/src/cm_api/api_client.py
+++ python/src/cm_api/api_client.py
@@ -29,7 +29,7 @@ __docformat__ = "epytext"
 LOG = logging.getLogger(__name__)
 
 API_AUTH_REALM = "Cloudera Manager"
-API_CURRENT_VERSION = 2
+API_CURRENT_VERSION = 3
 
 class ApiException(RestException):
   """
