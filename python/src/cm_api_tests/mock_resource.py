9e092c1e52581e79cc5a91909f7f5eb129973b4e
Marcelo Vanzin <vanzin@cloudera.com>
2013-01-15 14:32:46 -0800

    [py] Add unit tests for Python replication API bindinds.
    You read that right. Also includes some basic infrastructure
    to make writing new unit tests less painful.

---
+++ python/src/cm_api_tests/mock_resource.py
@@ -0,0 +1,66 @@
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cm_api import api_client
+from cm_api.resource import Resource
+
+class MockResource(Resource):
+  """
+  Allows code to control the behavior of a resource's "invoke" method for
+  unit testing.
+  """
+
+  def __init__(self, test, version=api_client.API_CURRENT_VERSION):
+    self._next_expect = None
+    self.test = test
+    self.version = version
+
+  @property
+  def base_url(self):
+    return ""
+
+  def invoke(self, method, relpath=None, params=None, data=None, headers=None):
+    """
+    Checks the expected input data and returns the appropriate data to the caller.
+    """
+    exp_method, exp_path, exp_params, exp_data, exp_headers, retdata = self._next_expect
+    self._next_expect = None
+
+    if exp_method is not None:
+      self.test.assertEquals(exp_method, method)
+    if exp_path is not None:
+      self.test.assertEquals(exp_path, relpath)
+    if exp_params is not None:
+      self.test.assertEquals(exp_params, params)
+    if exp_data is not None:
+      self.test.assertEquals(exp_data, data)
+    if exp_headers is not None:
+      self.test.assertEquals(exp_headers, headers)
+    return retdata
+
+  def expect(self, method, reqpath, params=None, data=None, headers=None,
+      retdata=None):
+    """
+    Sets the data to expect in the next call to invoke().
+
+    @param method: method to expect, or None for any.
+    @param reqpath: request path, or None for any.
+    @param params: query parameters, or None for any.
+    @param data: request body, or None for any.
+    @param headers: request headers, or None for any.
+    @param retdata: data to return from the invoke call.
+    """
+    self._next_expect = (method, reqpath, params, data, headers, retdata)
