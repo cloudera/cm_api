71f3f5dade717fe29d9a85b355d5b59f9ea3c8ae
Justin Hayes <jhayes@cloudera.com>
2014-04-24 13:52:10 -0700

    adding auto-deploy example

---
+++ python/examples/auto-deploy/README.md
@@ -0,0 +1,79 @@
+Cloudera Automatic Provisioning
+===============================
+
+These scripts deploy a Cloudera cluster, consisting of CDH and Cloudera Manager (CM). The cluster runs all of the available services in CDH, as well as the CM management services. You can use this as is to deploy a cluster or use it as a starting point to develop a more customized version that doesn't deploy all services or that uses different configurations.
+
+Some limitations are as follows:
+
+* It does not automatically validate the installation.
+* The configurations used have not been extensively tested. So while the cluster comes up with all greens from a health check perspective, you should still validate that everything works.
+* Not all management services are turned on, as some (ie Navigator) require a Cloudera Enterprise subscription.
+* Accumulo is currently not installed.
+* It currently works with CDH5.0. It can be modified to support CDH4.x, but it won't work without some modifications.
+
+Prerequisites:
+--------------
+- http://www.cloudera.com/content/cloudera-content/cloudera-docs/CM5/latest/Cloudera-Manager-Installation-Guide/cm5ig_cm_requirements.html
+
+
+Assumptions:
+------------
+- Running on a RHEL 6.x machine
+- All yum repos are set up. This is for common things like MySQL, expect, etc. A Cloudera repo is downloaded for Cloudera specific packages.
+- The clouderaconfig.ini file must contain settings applicable to a given environment (eg the proper hostnames, the ntp server, etc).
+
+
+Steps To Deploy a Cluster:
+--------------------------
+
+1. Ensure that clouderaconfig.ini is updated with the hostnames for the cluster, the root data directory (note that the default is to have one data directory, which is not recommended for production use), etc.
+
+2. Execute the following scripts at the same time. There are sleeps at the appropriate points to account for temporal dependencies amongst the slave and master processes. Something like Ansible could of course be used to do this in a less hackish way.
+    * sudo ./setup-slave.sh #run on all of the slave nodes (ie every node other than the master).
+    * sudo ./setup-master.sh #run on the CM master node.
+
+3. Go to http://$CM_MASTER_HOSTNAME:7180/cmf and log in (see the clouderaconfig.ini file for the credentials) to view the cluster.
+
+Tests to Validate the Installation:
+-----------------------------------
+
+```
+#Create hive table
+hive shell
+hive> CREATE EXTERNAL TABLE justin(id INT, name STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' STORED AS TEXTFILE LOCATION '/tmp/justin';
+hive> exit;
+
+#Create HDFS file
+echo "1 justin" > /tmp/test
+hadoop fs -put /tmp/test /tmp/justin/
+
+#Query hive table
+hive shell
+hive> select * from justin;
+hive> exit;
+
+#Query same table but in impala
+impala-shell
+impala> invalidate metadata;
+impala> select * from justin;
+impala> exit;
+
+#Run mapreduce job
+hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/hadoop-examples.jar pi 10 10
+
+#Test hbase
+hbase shell
+hbase> create 'test', 'cf'
+hbase> list 'test'
+hbase> put 'test', 'row1', 'cf:a', 'value1'
+hbase> scan 'test'
+hbase> exit
+
+#Test spark
+echo "this is the end. the only end. my friend." > /tmp/sparkin
+hadoop fs -put /tmp/sparkin /tmp/
+spark-shell
+scala> val file = sc.textFile("hdfs://$NAMENODE:8020/tmp/sparkin")
+scala> val counts = file.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+scala> counts.saveAsTextFile("hdfs://$NAMENODE:8020/tmp/sparkout")
+```
---
+++ python/examples/auto-deploy/setup-master.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# This should be run on the host on which the CM server is to run.
+
+# Set up some vars
+config_file=clouderaconfig.ini
+cm_server_host=`grep cm.host $config_file | awk -F'=' '{print $2}'`
+ntp_server=`grep ntp.server $config_file | awk -F'=' '{print $2}'`
+# This should be set to whatever HIVE_HMS_HOST is set to in deploycloudera.py
+hive_metastore_host=`grep hive.metastore.host $config_file | awk -F'=' '{print $2}'`
+hive_metastore_password=`grep hive.metastore.password $config_file | awk -F'=' '{print $2}'`
+
+# Prep Cloduera repo
+sudo yum -y install wget
+wget http://archive.cloudera.com/cm5/redhat/6/x86_64/cm/cloudera-manager.repo
+sudo mv cloudera-manager.repo /etc/yum.repos.d/
+
+# Turn off firewall
+sudo service iptables stop
+
+#Turn off SELINUX
+sudo echo 0 >/selinux/enforce
+
+#Set up NTP
+sudo yum -y install ntp
+sudo chkconfig ntpd on
+sudo ntpdate $ntp_server
+sudo /etc/init.d/ntpd start
+
+# Set up python
+sudo rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+sudo yum -y install python-pip
+sudo pip install cm_api
+
+# Set up MySQL
+wget http://archive.cloudera.com/cdh5/cdh/5/hive-0.12.0-cdh5.0.0.tar.gz
+tar zxf hive-0.12.0-cdh5.0.0.tar.gz
+sudo yum -y install mysql-server expect
+sudo service mysqld start
+sudo /sbin/chkconfig mysqld on
+sudo /bin/echo -e "\nY\n$hive_metastore_password\n$hive_metastore_password\nY\nn\nY\nY\n" > /tmp/answers
+sudo /usr/bin/mysql_secure_installation < /tmp/answers
+sudo rm /tmp/answers
+mysql -uroot -p$hive_metastore_password --execute="CREATE DATABASE metastore; USE metastore; SOURCE ./hive-0.12.0-cdh5.0.0/scripts/metastore/upgrade/mysql/hive-schema-0.12.0.mysql.sql;"
+mysql -uroot -p$hive_metastore_password --execute="CREATE USER 'hive'@'$hive_metastore_host' IDENTIFIED BY '$hive_metastore_password';"
+mysql -uroot -p$hive_metastore_password --execute="REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'hive'@'$hive_metastore_host';"
+mysql -uroot -p$hive_metastore_password --execute="GRANT SELECT,INSERT,UPDATE,DELETE,LOCK TABLES,EXECUTE ON metastore.* TO 'hive'@'$hive_metastore_host';"
+mysql -uroot -p$hive_metastore_password --execute="FLUSH PRIVILEGES;"
+mysql -uroot -p$hive_metastore_password --execute="create database oozie; grant all privileges on oozie.* to 'oozie'@'localhost' identified by '$hive_metastore_password'; grant all privileges on oozie.* to 'oozie'@'%' identified by '$hive_metastore_password';"
+
+#Make sure DNS is set up properly so all nodes can find all other nodes
+
+# For master
+sudo yum -y install cloudera-manager-agent cloudera-manager-daemons cloudera-manager-server cloudera-manager-server-db-2
+sudo service cloudera-scm-server-db start
+sudo service cloudera-scm-server start
+sudo sed -i.bak -e"s%server_host=localhost%server_host=$cm_server_host%" /etc/cloudera-scm-agent/config.ini
+sudo service cloudera-scm-agent start
+
+# Prep work before calling the Cloudera provisioning script.
+firehostdbpassword=`grep com.cloudera.cmf.ACTIVITYMONITOR.db.password /etc/cloudera-scm-server/db.mgmt.properties | awk -F'=' '{print $2}'`
+navigatordbpassword=`grep com.cloudera.cmf.NAVIGATOR.db.password /etc/cloudera-scm-server/db.mgmt.properties | awk -F'=' '{print $2}'`
+headlampdbpassword=`grep com.cloudera.cmf.REPORTSMANAGER.db.password /etc/cloudera-scm-server/db.mgmt.properties | awk -F'=' '{print $2}'`
+
+# Sleep for a while to give the agents enough time to check in with the master.
+# Or better yet, make a dependency so that the slave setup cripts don't start until now and the rest of this script doesn't finish until the slaves finish.
+sleep_time=180
+echo "Sleeping for $sleep_time seconds so managed cluster nodes can get set up."
+sleep $sleep_time
+echo "Done sleeping. Deploying cluster now."
+
+# Execute script to deploy Cloudera cluster
+sudo python deploycloudera.py -i$hive_metastore_password -f$firehostdbpassword -n$navigatordbpassword -r$headlampdbpassword
+
+# Now stop the cluster gracefully if necessary; ie if all servers are automatically rebooted at the end of the provisioning process
+#sudo python stopcloudera.py
