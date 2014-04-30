e8c8db4b4cc96addee06c5fdd817d170e6bae86e
Justin Hayes <jhayes@cloudera.com>
2014-04-30 12:48:46 -0700

    typos and updated gitignore for PyDev

--- python/examples/auto-deploy/setup-slave.sh
+++ python/examples/auto-deploy/setup-slave.sh
@@ -8,7 +8,7 @@ config_file=clouderaconfig.ini
 cm_server_host=`grep cm.host $config_file | awk -F'=' '{print $2}'`
 ntp_server=`grep ntp.server $config_file | awk -F'=' '{print $2}'`
 
-# Prep Cloduera repo
+# Prep Cloudera repo
 sudo yum -y install wget
 wget http://archive.cloudera.com/cm5/redhat/6/x86_64/cm/cloudera-manager.repo
 sudo mv cloudera-manager.repo /etc/yum.repos.d/
@@ -16,7 +16,7 @@ sudo mv cloudera-manager.repo /etc/yum.repos.d/
 # Turn off firewall
 sudo service iptables stop
 
-#Turn off SELINUX
+# Turn off SELINUX
 sudo echo 0 >/selinux/enforce
 
 #Set up NTP
@@ -25,12 +25,12 @@ sudo chkconfig ntpd on
 sudo ntpdate $ntp_server
 sudo /etc/init.d/ntpd start
 
-#Make the mysql driver available to hive
+# Make the mysql driver available to hive
 sudo yum -y install mysql-connector-java
 sudo mkdir -p /usr/lib/hive/lib/
 sudo ln -s /usr/share/java/mysql-connector-java.jar /usr/lib/hive/lib/mysql-connector-java.jar
 
-#Make sure DNS is set up properly so all nodes can find all other nodes
+# Make sure DNS is set up properly so all nodes can find all other nodes
 
 # For slaves
 sudo yum -y install cloudera-manager-agent cloudera-manager-daemons
