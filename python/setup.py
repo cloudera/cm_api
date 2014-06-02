755b53047992e102229441e1a3b689d49e17e6c4
Vikram Srivastava <vikrams@cloudera.com>
2014-06-01 22:41:34 -0700

    [cm_api] Bump the version to v7

--- python/setup.py
+++ python/setup.py
@@ -35,7 +35,7 @@ if platform == 'darwin':
 
 setup(
   name = 'cm_api',
-  version = '6.0.1',    # Compatible with API v6 (CM 5.0 beta 2)
+  version = '7.0.0',    # Compatible with API v7 (CM 5.1)
   packages = find_packages('src', exclude=['cm_api_tests']),
   package_dir = {'cm_api': 'src/cm_api',
                  'cm_shell': 'src/cm_shell'},
