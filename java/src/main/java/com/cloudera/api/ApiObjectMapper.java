7f6d49d9377bd67d32df753d713d66f91713bb5a
Jon Drew <jrdrew@gmail.com>
2016-08-19 16:16:23 -0400

    updating the version of Jackson to 2.7

--- java/src/main/java/com/cloudera/api/ApiObjectMapper.java
+++ java/src/main/java/com/cloudera/api/ApiObjectMapper.java
@@ -16,10 +16,11 @@
 
 package com.cloudera.api;
 
-import com.fasterxml.jackson.databind.AnnotationIntrospector;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 
 import java.text.DateFormat;
@@ -38,9 +39,9 @@ public class ApiObjectMapper extends ObjectMapper {
 
     // Allow JAX-B annotations.
     setAnnotationIntrospector(
-        new AnnotationIntrospector.Pair(
+        new AnnotationIntrospectorPair(
             getSerializationConfig().getAnnotationIntrospector(),
-            new JaxbAnnotationIntrospector()));
+            new JaxbAnnotationIntrospector(TypeFactory.defaultInstance())));
 
     // Make Jackson respect @XmlElementWrapper.
     enable(MapperFeature.USE_WRAPPER_NAME_AS_PROPERTY_NAME);
