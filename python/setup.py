d799c233f6ad2f4d28ec00975ceea8c4a79daee4
Darren Lo <dlo@cloudera.com>
2017-01-25 13:59:47 -0800

    [api] Bump version to v16 for 5.11
    This change is not intended for master.
    
    No-Crossport: master
    
    Testing:
    * make test (from python dir)
    * mvn install (from java dir)

--- python/setup.py
+++ python/setup.py
@@ -44,7 +44,7 @@ src_dir = os.path.normpath(os.path.join(base_dir, 'src'))
 
 setup(
   name = 'cm_api',
-  version = '15.0.0',    # Compatible with API v15 (CM 5.10)
+  version = '16.0.0',    # Compatible with API v16 (CM 5.11)
   packages = find_packages(src_dir, exclude=['cm_api_tests']),
   package_dir = {'': src_dir },
   zip_safe = True,
