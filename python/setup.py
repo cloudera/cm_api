ac344799d472c771c7de00c3637bbf0f4159300d
Nachiket Vaidya <nvaidya@cloudera.com>
2017-10-31 18:13:06 -0700

    [api] OPSAPS-43367 Bump CM API to v19 for 5.14 (python)
    Update Python bindings.

--- python/setup.py
+++ python/setup.py
@@ -44,7 +44,7 @@ src_dir = os.path.normpath(os.path.join(base_dir, 'src'))
 
 setup(
   name = 'cm_api',
-  version = '18.0.0',    # Compatible with API v18 (CM 5.13)
+  version = '19.0.0',    # Compatible with API v19 (CM 5.14)
   packages = find_packages(src_dir, exclude=['cm_api_tests']),
   package_dir = {'': src_dir },
   zip_safe = True,
